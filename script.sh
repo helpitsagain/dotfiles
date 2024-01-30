eb43f9af0fdeec0ebb445ee5c40d9d2dc4ec4caa
Luc <lucmichels94@gmail.com>
2024-01-29 23:50:38 -0300

    adding script.sh

---
+++ script.sh
@@ -0,0 +1,20 @@
+#!bin/bash
+
+echo "Copying .bashrc to $HOME"
+cp -b home/bashrc $HOME/.bashrc
+
+echo "Copying .bash_aliases to $HOME"
+cp -b home/bash_aliases $HOME/.bash_aliases
+
+echo "Copying lazy.nvim to $HOME"
+cp -rb home/lazy.nvim $HOME/
+
+echo "Copying config/nvim to $HOME/.config"
+cp -rb ./config/nvim $HOME/.config/nvim
+
+echo "Copying config/tmux to $HOME/.config"
+cp -rb ./config/tmux $HOME/.config/tmux
+
+echo "Copying config/ranger to $HOME/.config"
+cp -rb ./config/ranger $HOME/.config/ranger
+export RANGER_LOAD_DEFAULT_RC=false
