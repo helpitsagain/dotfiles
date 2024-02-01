7248c9be33447a7307fe64ddb0cf6fb69cd171ff
Luc <lucmichels94@gmail.com>
2024-02-01 06:31:27 -0300

    updating config/nvim, should stop giving error messages from lazy now

--- config/nvim/lua/custom/mappings.lua
+++ config/nvim/lua/custom/mappings.lua
@@ -11,14 +11,7 @@ M.general = {
         require("conform").format()
       end,
       "formatting",
-    },
-
-    {
-      ["<C-h>"] = { "<cmd> TmuxNavigateLeft<CR>", "window left" },
-      ["<C-l>"] = { "<cmd> TmuxNavigateRight<CR>", "window right" },
-      ["<C-j>"] = { "<cmd> TmuxNavigateDown<CR>", "window down" },
-      ["<C-k>"] = { "<cmd> TmuxNavigateUp<CR>", "window up" },
-    },
+    }
 
   },
   v = {
