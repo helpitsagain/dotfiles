d9d7c8fc9cec833fa4f13c8a441ea4c4fb2de366
Luc <lucmichels94@gmail.com>
2024-02-02 20:47:52 -0300

    updating script.sh

--- script.sh
+++ script.sh
@@ -1,64 +1,116 @@
-#!bin/bash
+#!/bin/bash
 
-current_dir=$(pwd)
+current_dir="$(pwd)"
 
-dotfiles_dir=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
+dotfiles_dir="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
 
-read -p "Do you want to install yay? [y/N] " -i "N" install_yay
-case $install_yay in
-"Y" | "y")
-  cd $HOME ;
-  sudo pacman -S --needed git base-devel ;
-  git clone https://aur.archlinux.org/yay.git ;
-  cd yay && makepkg -si ;
-  sleep 1 ;
-  yay -Y --gendb ;
-  yay -Syu --devel ;
-  cd $dotfiles_dir ;;
-"N" | "n" | "") echo "Skipping yay" ;;
-esac
+# distro=$(lsb_release -i | cut -f 2-)
+if command -v apt &>/dev/null; then
+  distro="Ubuntu"
+elif command -v pacman &>/dev/null; then
+  distro="Arch"
+elif command -v yum &>/dev/null; then
+  distro="RHEL"
+fi
 
-read -p "Do you want to install packages? [y/N] " -i "N" install_pkgs
-case $install_pkgs in
-"Y" | "y")
-  echo "Installing packages" ;
-  sudo pacman -S nodejs npm neovim tmux ranger ;;
-"N" | "n" | "") echo "Skipping packages" ;;
-esac
+if [ "$distro" == "Arch" ]; then
+  while true; do
+    read -p "Do you want to install yay? [y/N] " install_yay
+    case "$install_yay" in
+    "y")
+      cd "$HOME"
+      sudo pacman -S --needed git base-devel
+      git clone https://aur.archlinux.org/yay.git
+      cd yay && makepkg -si
+      sleep 1
+      yay -Y --gendb
+      yay -Syu --devel
+      cd "$dotfiles_dir"
+      break
+      ;;
+    "n" | "")
+      echo "Skipping yay"
+      break
+      ;;
+    *)
+      echo "Invalid input. Please enter either \"y\" or \"n\", or leave input blank."
+      ;;
+    esac
+  done
+fi
 
-read -p "Do you want to install oh-my-posh? [y/N] " -i "n" install_omp
-case $install_omp in
-"Y" | "y")
-  echo "Installing oh-my-posh" ;
-  sudo pacman install unzip ;
-  mkdir $HOME/bin ;
-  curl -s https://ohmyposh.dev/install.sh | bash -s -- -d $HOME/bin ;
-  cp -r config/oh-my-posh $HOME/.config/ ;;
-"N" | "n" | "") echo "Skipping oh-my-posh" ;;
-esac
+while true; do
+  read -p "Do you want to install packages? [y/N] " install_pkgs
+  case "${install_pkgs,,}" in
+  "y")
+    echo "Installing packages"
+    if [ "$distro" == "Ubuntu" ]; then
+      sudo apt install nodejs npm neovim tmux ranger neofetch
+    elif [ "$distro" == "Arch" ]; then
+      sudo pacman -S nodejs npm neovim tmux ranger neofetch
+    elif [ "$distro" == "RHEL" ]; then
+      sudo yum install nodejs npm neovim tmux ranger neofetch
+    fi
+    break
+    ;;
+  "n" | "")
+    echo "Skipping packages"
+    break
+    ;;
+  *)
+    echo "Invalid input. Please enter either \"y\" or \"n\", or leave input blank."
+    ;;
+  esac
+done
 
-cd $dotfiles_dir
+while true; do
+  read -p "Do you want to install oh-my-posh? [y/N] " install_omp
+  case "${install_omp,,}" in
+  "y")
+    echo "Installing oh-my-posh"
+    if [ "$distro" == "Ubuntu" ]; then
+      sudo apt install unzip
+    elif [ "$distro" == "Arch" ]; then
+      sudo pacman -S unzip
+    elif [ "$distro" == "RHEL" ]; then
+      sudo yum install unzip
+    fi
+    mkdir "$HOME/bin"
+    curl -s https://ohmyposh.dev/install.sh | bash -s -- -d "$HOME/bin"
+    cp -r config/oh-my-posh "$HOME/.config/"
+    break
+    ;;
+  "n" | "")
+    echo "Skipping oh-my-posh"
+    break
+    ;;
+  *)
+    echo "Invalid input. Please enter either \"y\" or \"n\", or leave input blank."
+    ;;
+  esac
+done
+
+cd "$dotfiles_dir"
 
 echo "Copying .bashrc to $HOME"
-cp -b home/bashrc $HOME/.bashrc
+cp -b home/bashrc "$HOME/.bashrc"
 
 echo "Copying .bash_aliases to $HOME"
-cp -b home/bash_aliases $HOME/.bash_aliases
+cp -b home/bash_aliases "$HOME/.bash_aliases"
 
-source $HOME/.bashrc
-source $HOME/.bash_aliases
+source "$HOME/.bashrc"
+source "$HOME/.bash_aliases"
 
 echo "Copying config/nvim to $HOME/.config"
-cp -rb ./config/nvim $HOME/.config/
+cp -rb ./config/nvim "$HOME/.config/"
 
 echo "Copying config/tmux to $HOME/.config"
-cp -rb ./config/tmux $HOME/.config/
+cp -rb ./config/tmux "$HOME/.config/"
 echo "Installing tmux plugin manager"
-git clone https://github.com/tmux-plugins/tpm $HOME/.tmux/plugins/tpm
+git clone https://github.com/tmux-plugins/tpm "$HOME/.tmux/plugins/tpm"
 
 echo "Copying config/ranger to $HOME/.config"
-cp -rb ./config/ranger $HOME/.config/
+cp -rb ./config/ranger "$HOME/.config/"
 export RANGER_LOAD_DEFAULT_RC=false
 
-cd $current_dir
-
+cd "$current_dir"
