faf07c9e02b9415bd40507f4748d98cf091bfa18
Luc <lucasm@bempromotora.com.br>
2024-02-07 16:50:06 -0300

    fix: resolving issue in vim-tmux-navigator

--- .config/nvim/lua/custom/mappings.lua
+++ .config/nvim/lua/custom/mappings.lua
@@ -11,7 +11,12 @@ M.general = {
         require("conform").format()
       end,
       "formatting",
-    }
+    },
+
+    ["<C-h>"] = { "<cmd> TmuxNavigateLeft<CR>", "window left" },
+    ["<C-j>"] = { "<cmd> TmuxNavigateDown<CR>", "window down" },
+    ["<C-k>"] = { "<cmd> TmuxNavigateUp<CR>", "window up" },
+    ["<C-l>"] = { "<cmd> TmuxNavigateRight<CR>", "window right" },
 
   },
   v = {
