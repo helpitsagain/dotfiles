80ca4d06f5833278236d50ef9bdc134c93e982f7
Luc <lucmichels94@gmail.com>
2024-02-18 04:37:03 -0300

    refactor: (WIP) cleaning up install.py

--- scripts/install.py
+++ scripts/install.py
@@ -1,25 +1,6 @@
 #!/usr/bin/env python
 import os
-import subprocess
 import platform
-
-def get_dotfiles_dir():
-    # shell command to get script's dir
-    result = subprocess.run(['dirname', os.path.realpath(__file__)], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
-
-    # check if command was successful
-    if result.returncode == 0: 
-        script_dir = result.stdout.strip()
-        dotfiles_dir = script_dir[:script_dir.rindex('/')] # cuts off script folder from path
-        return dotfiles_dir
-
-    print(f'Error: {result.stderr}')
-    return ''
-
-def get_current_dir():
-    result = subprocess.run('pwd', capture_output=True, text=True)
-    output = result.stdout.strip()
-    return output
 
 def get_distro():
     info = platform.freedesktop_os_release()
@@ -27,18 +8,7 @@ def get_distro():
         return info['ID_LIKE']
     return info['ID']
 
-def yay_install():
-    os.chdir(home_dir)
-    os.system('git clone https://aur.archlinux.org/yay.git')
-    os.chdir('yay')
-    os.system('makepkg -si')
-    os.system('sleep 1')
-    os.system('yay -Y --gendb')
-    os.system('yay -Syu --devel')
-
 home_dir: str = os.path.expanduser('~')
-dotfiles_dir: str = get_dotfiles_dir()
-current_dir: str = get_current_dir()
 distro: str = get_distro()
 
 # install yay 
@@ -46,7 +16,13 @@ if distro == 'arch':
     while True:
         install_yay = input('Do you want to install yay? [y/N] ').capitalize().strip()
         if install_yay == 'Y':
-            yay_install()
+            os.chdir(home_dir)
+            os.system('git clone https://aur.archlinux.org/yay.git')
+            os.chdir('yay')
+            os.system('makepkg -si')
+            os.system('sleep 1')
+            os.system('yay -Y --gendb')
+            os.system('yay -Syu --devel')
             break
 
         if install_yay == 'N' or install_yay == '':
