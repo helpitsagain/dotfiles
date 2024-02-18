58ff1c0bd48ffa8eee9c33fb632ef86b460c56db
Luc <lucmichels94@gmail.com>
2024-02-18 03:20:38 -0300

    feat: adding python formatters

--- .config/nvim/lua/custom/configs/conform.lua
+++ .config/nvim/lua/custom/configs/conform.lua
@@ -9,17 +9,19 @@ local options = {
 		css = { "prettier" },
 		html = { "prettier" },
 
+    python = { "isort", "black" },
+
 		sh = { "shfmt" },
 	},
 
   -- adding same formatter for multiple filetypes can look too much work for some
   -- instead of the above code you could just use a loop! the config is just a table after all!
 
-	-- format_on_save = {
-	--   -- These options will be passed to conform.format()
-	--   timeout_ms = 500,
-	--   lsp_fallback = true,
-	-- },
+	format_on_save = {
+	  -- These options will be passed to conform.format()
+	  timeout_ms = 500,
+	  lsp_fallback = true,
+	},
 }
 
 require("conform").setup(options)
