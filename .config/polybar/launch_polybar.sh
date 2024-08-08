e07cafe426d5026f7112c40a58256f9d4ea000ba
Luc Michels <lucmichels94@gmail.com>
2024-08-08 14:25:49 -0300

    feat: fixing polybar spawning two bars instead of one

--- .config/polybar/launch_polybar.sh
+++ .config/polybar/launch_polybar.sh
@@ -1,20 +1,22 @@
-
 # terminate polybar
 killall -q polybar
 
 # wait until all processes shut down
-while pgrep -u $UID -x polybar > /dev/null; do sleep 1; done
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 desktop=$(echo $DESKTOP_SESSION)
 
 count=$(xrandr --query | grep " connected" | cut -d" " -f1 | wc -l)
 
-case $desktop in i3|/usr/share/xsessions/i3.desktop)
-  if type "xrandr" > /dev/null; then
-    for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-      MONITOR=$m polybar --reload toph &
-    done
-  else
-    polybar --reload toph &
-  fi
-esac
+# case $desktop in i3 | /usr/share/xsessions/i3.desktop)
+#   if type "xrandr" >/dev/null; then
+#     for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+#       MONITOR=$m polybar --reload toph &
+#     done
+#   else
+#     polybar --reload toph &
+#   fi
+#   ;;
+# esac
+
+polybar --reload toph &
