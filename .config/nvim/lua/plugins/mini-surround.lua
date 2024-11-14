a0ee1faae85db180dfaf2c9baf4ed7fcfe006773
Lucas Michels <lucmichels94@gmail.com>
2024-11-14 17:29:25 -0300

    nvim: feat: adding some custom surroundings to mini.surround

--- .config/nvim/lua/plugins/mini-surround.lua
+++ .config/nvim/lua/plugins/mini-surround.lua
@@ -1,16 +1,36 @@
 return {
   {
     'echasnovski/mini.surround',
-    opts = {
-      mappings = {
-        add = 'gsa',
-        delete = 'gsd',
-        find = 'gsf',
-        find_left = 'gsF',
-        highlight = 'gsh',
-        replace = 'gsr',
-        update_n_lines = 'gsn',
-      },
-    },
+    config = function()
+      require('mini.surround').setup({
+        mappings = {
+          add = 'gsa',
+          delete = 'gsd',
+          find = 'gsf',
+          find_left = 'gsF',
+          highlight = 'gsh',
+          replace = 'gsr',
+          update_n_lines = 'gsn',
+        },
+
+        custom_surroundings = {
+          d = {
+            output = function()
+              return { left = '<div>', right = '</div>' }
+            end,
+          },
+          c = {
+            output = function()
+              return { left = '/*', right = '*/' }
+            end,
+          },
+          s = {
+            output = function()
+              return { left = '${', right = '}' }
+            end,
+          },
+        },
+      })
+    end,
   },
 }
