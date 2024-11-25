8546d053ced575e6959da80f527d27d7042b5432
Lucas Michels <lucmichels94@gmail.com>
2024-11-25 15:34:09 -0300

    nvim: chore: some very minor changes of theme

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -10,11 +10,11 @@ return {
         dark = 'mocha',
       },
       transparent_background = true, -- disables setting the background color.
-      show_end_of_buffer = false, -- shows the '~' characters after the end of buffers
-      term_colors = false, -- sets terminal colors (e.g. `g:terminal_color_0`)
+      show_end_of_buffer = true, -- shows the '~' characters after the end of buffers
+      term_colors = true, -- sets terminal colors (e.g. `g:terminal_color_0`)
       dim_inactive = {
         enabled = true, -- dims the background color of inactive window
-        shade = 'dark',
+        shade = 'light',
         percentage = 0.15, -- percentage of the shade to apply to the inactive window
       },
       no_italic = false, -- Force no italic
@@ -42,12 +42,14 @@ return {
         cmp = true,
         gitsigns = true,
         nvimtree = true,
+        neotree = true,
         treesitter = true,
         notify = false,
         mini = {
           enabled = true,
           indentscope_color = '',
         },
+        noice = true,
         -- For more plugins integrations please scroll down (https://github.com/catppuccin/nvim#integrations)
       },
     })
