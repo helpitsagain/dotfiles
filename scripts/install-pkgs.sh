6833f8c5e8fb34b730be5287ee24bceab0daadba
Luc Michels <lucmichels94@gmail.com>
2025-03-08 23:55:04 -0300

    feat: updating install script

--- scripts/install-pkgs.sh
+++ scripts/install-pkgs.sh
@@ -8,6 +8,8 @@ if command -v apt &>/dev/null; then
   distro="Ubuntu"
 elif command -v pacman &>/dev/null; then
   distro="Arch"
+elif command -v dnf &>/dev/null; then
+  distro="Fedora"
 elif command -v yum &>/dev/null; then
   distro="RHEL"
 fi
@@ -20,8 +22,8 @@ if [ "$distro" = "Arch" ]; then
     [Yy]*)
       cd "$HOME";
       sudo pacman -S --needed git base-devel;
-      git clone https://aur.archlinux.org/yay.git;
-      cd yay && makepkg -si;
+      git clone https://aur.archlinux.org/yay.git "$HOME/yay";
+      cd "$HOME/yay" && makepkg -si;
       sleep 1;
       yay -Y --gendb;
       yay -Syu --devel;
@@ -29,7 +31,7 @@ if [ "$distro" = "Arch" ]; then
       break
       ;;
     [Nn]* | "")
-      echo "Skipping yay";
+      echo "Skipping yay installation";
       break
       ;;
     *)
@@ -46,17 +48,31 @@ while true; do
   [Yy]*)
     echo "Installing dependencies";
     if [ "$distro" = "Ubuntu" ]; then
-      sudo apt install -y git stow unzip nodejs npm
+      sudo apt install -y git stow unzip nodejs npm zsh
     elif [ "$distro" = "Arch" ]; then
-      sudo pacman -S git stow unzip nodejs npm
-    elif [ "$distro" = "RHEL" ]; then
-      sudo yum install -y git stow unzip nodejs npm
+      sudo pacman -S git stow unzip nodejs npm zsh
+    #  RHEL/Fedora not implemented yet
+    # elif [ "$distro" = "RHEL" ]; then
+    #   sudo yum install -y git stow unzip nodejs npm zsh
     fi
     echo "Dependencies installed successfully!";
+
+    echo "Set zsh as default shell? [Y/n]"
+    read set_default_zsh;
+    case "$set_default_zsh" in
+      [Yy]* | "")
+        chsh -s $(which zsh)
+        echo "You should log out and back in to see the change!";
+        break
+        ;;
+      [Nn]*)
+        echo "Skipping zsh installation";
+        break
+        ;;
     break
     ;;
   [Nn]* | "")
-    echo "Skipping dependencies";
+    echo "Skipping dependency installation";
     break
     ;;
   *)
@@ -67,24 +83,26 @@ done
 
 while true; do
   echo "Do you want to install packages? [y/N]";
-  read install_pkgs""
+  read install_pkgs
   case "$install_pkgs" in
   [Yy]*)
     echo "Installing packages";
     if [ "$distro" = "Ubuntu" ]; then
+      sudo apt install -y software-properties-common
       sudo add-apt-repository ppa:neovim-ppa/unstable
       sudo apt update
       sudo apt install -y neovim tmux ranger neofetch
     elif [ "$distro" = "Arch" ]; then
       sudo pacman -S neovim tmux ranger neofetch
-    elif [ "$distro" = "RHEL" ]; then
-      sudo yum install -y neovim tmux ranger neofetch
+    #  RHEL/Fedora not implemented yet
+    # elif [ "$distro" = "RHEL" ]; then
+    #   sudo yum install -y neovim tmux ranger neofetch
     fi
     echo "Packages installed successfully!";
     break
     ;;
   [Nn]* | "")
-    echo "Skipping packages";
+    echo "Skipping package installation";
     break
     ;;
   *)
@@ -100,22 +118,38 @@ while true; do
   [Yy]*)
     echo "Installing FiraCode";
     if [ "$distro" = "Ubuntu" ]; then
-      mkdir -p "$HOME/local/share/fonts"
+      mkdir -p "$HOME/.local/share/fonts"
       cd "$HOME/.local/share/fonts"
-      curl -fLO https://github.com/ryanoasis/nerd-fonts/raw/HEAD/patched-fonts/DroidSansMono/DroidSansMNerdFont-Regular.otf
+      curl -fLO https://github.com/ryanoasis/nerd-fonts/releases/latest/download/FiraCode.zip
+      unzip FiraCode.zip -d "$HOME/.local/share/fonts"
+      rm FiraCode.zip
+      fc-cache -fv
     elif [ "$distro" = "Arch" ]; then
-      yay -S ttf-firacode-nerd
-    elif [ "$distro" = "RHEL" ]; then
-      mkdir -p $HOME/.local/share/fonts
-      cd $HOME/.local/share/fonts
-      curl -fLO https://github.com/ryanoasis/nerd-fonts/raw/HEAD/patched-fonts/DroidSansMono/DroidSansMNerdFont-Regular.otf
+      if command -v yay &>/dev/null; then
+        yay -S ttf-firacode-nerd
+      else
+        mkdir -p "$HOME/.local/share/fonts"
+        cd "$HOME/.local/share/fonts"
+        curl -fLO https://github.com/ryanoasis/nerd-fonts/releases/latest/download/FiraCode.zip
+        unzip FiraCode.zip -d "$HOME/.local/share/fonts"
+        rm FiraCode.zip
+      fi
+      fc-cache -fv
+    #  RHEL/Fedora not implemented yet
+    # elif [ "$distro" = "RHEL" ]; then
+    #   mkdir -p "$HOME/.local/share/fonts"
+    #   cd "$HOME/.local/share/fonts"
+    #   curl -fLO https://github.com/ryanoasis/nerd-fonts/releases/latest/download/FiraCode.zip
+    #   unzip FiraCode.zip -d "$HOME/.local/share/fonts"
+    #   rm FiraCode.zip
+    #   fc-cache -fv
     fi
     cd "$dotfiles_dir"
     echo "FiraCode font installed successfully!";
     break
     ;;
   [Nn]* | "")
-    echo "Skipping FiraCode";
+    echo "Skipping FiraCode installation";
     break
     ;;
   *)
@@ -123,5 +157,3 @@ while true; do
     ;;
   esac
 done
-
-
